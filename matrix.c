05286a506c84648c4604520bc660e1a916db13d1
Arsen <arsen.mkhitaryan.mkh@gmail.com>
2025-04-10 17:41:34 +0400

    c example

---
+++ matrix.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main (){
+
+	int matrix [3][3];
+	
+	int i,j;
+
+	for (i = 0; i < 3; i++) {		
+		for (j = 0; j < 3; j++) {
+			matrix[i][j] = i * 3 + j + 1;
+}
+
+	}
+	
+	for(i = 0;  i < 3; i++){
+		
+		for(j = 0; j < 3; j++ ){
+		
+			printf("%d", matrix[i][j]);
+
+		}
+
+printf("\n");
+
+}
+
+}
